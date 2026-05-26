10ad93bf39587adc71f445007386c24320a4e432
Erick Mattos <erickmattos31@gmail.com>
2026-05-26 16:04:57 -0300

    Initial check-in

---
+++ Assets/Script/WalkTalkSom.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class WalkTalkSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Play();
+        }
+    }
+}
